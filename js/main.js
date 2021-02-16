970c2d439431090d9444bd2c82514bcb1f2b1c15
Ruslan Troyan <300493rt@gmail.com>
2021-02-16 13:16:45 +0200

    Испправил переменные на const, названия переменных

--- js/main.js
+++ js/main.js
@@ -1,32 +1,28 @@
 
 // Источник https://learn.javascript.ru/task/random-int-min-max
 
-let minValue = 1;
-let maxValue = 14;
+const MIN_VALUE = 1;
+const MAX_VALUE = 14;
 
-let randomInteger = function (min, max) {
+const randomInteger = function (min, max) {
   // получить случайное число от (min-0.5) до (max+0.5)
   if (min < max) {
-    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    const rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
   } else {
-    alert('Значение ' + min + 'должно быть больше ' + max)
+    alert('Значение ' + min + ' должно быть больше ' + max)
   }
 };
 
-randomInteger(minValue, maxValue);
+randomInteger(MIN_VALUE, MAX_VALUE);
 
 // Длина строки (авторская=))
 
-let stringComment = ('Определенное количество символов');
-let lengthLimit = 140;
+const stringComment = 'Определенное количество символов';
+const lengthLimit = 140;
 
-let returnLengthString = function (workString, maxLength) {
-  if (workString.length <= maxLength) {
-    return true;
-  } else {
-    return false;
-  }
+const returnLengthString = function (current, max) {
+  return current.length <= max;
 };
 
 returnLengthString(stringComment, lengthLimit);
