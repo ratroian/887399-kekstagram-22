9bf6b33d59cc5832784253746e13fc3cb69d0dfe
Ruslan Troyan <300493rt@gmail.com>
2021-02-18 23:29:16 +0200

    Сгенерировал 25 обьетков. Нужно доработать на предмет уникальности

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // Источник https://learn.javascript.ru/task/random-int-min-max
 
 const MIN_VALUE = 1;
-const MAX_VALUE = 14;
+const MAX_VALUE = 10;
 
 const randomInteger = function (min, max) {
   // получить случайное число от (min-0.5) до (max+0.5)
@@ -16,13 +16,79 @@ const randomInteger = function (min, max) {
 
 randomInteger(MIN_VALUE, MAX_VALUE);
 
-// Длина строки (авторская=))
+const ID_OBJECTS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+const LINKS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+// const likes = _.random(15, 200);
+const COMENTS = ['Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
-const stringComment = 'Определенное количество символов';
-const lengthLimit = 140;
+const createObject = () => {
+  const randomIndex = _.random(0, ID_OBJECTS.length - 1);
+  const randomLinks = _.random(0, LINKS.length - 1);
+  const randomComents = _.random(0, COMENTS.length - 1);
 
-const returnLengthString = function (current, max) {
-  return current.length <= max;
+  return {
+    id: ID_OBJECTS[randomIndex],
+    url: 'photos/' + LINKS[randomLinks] + '.jpg',
+    description: 'Нормальное описание к фото №' + LINKS[randomLinks],
+    likes: _.random(15, 200),
+    comments: {
+      id: _.random(1, 200),
+      avatar: 'img/avatar' + _.random(1, 6) + '.svg',
+      message: COMENTS[randomComents],
+      name: 'Артем' + _.random(1, 6),
+    },
+  };
 };
 
-returnLengthString(stringComment, lengthLimit);
+console.log(createObject());
+
+const TOTAL_OBJECT = 25;
+
+const similarObjects = new Array(TOTAL_OBJECT).fill(null).map(() => createObject());
+
+console.log(similarObjects);
+
+
+
+// // Длина строки (авторская=))
+
+// const stringComment = 'Определенное количество символов';
+// const lengthLimit = 140;
+
+// const returnLengthString = function (current, max) {
+//   return current.length <= max;
+// };
+
+// returnLengthString(stringComment, lengthLimit);
+
+// // Создаю пустые массивы
+// const idObjects = [];
+// const links = [];
+// const arrayLength = 25;
+// const likes = [];
+
+// // Вношу данные в массивы
+// for(i = 0; i < arrayLength; i++) Links.push(i);
+// for(i = 0; i < arrayLength; i++) idObjects.push(i);
+// for(i = 15; i < 250; i++) likes.push(i);
+
+
+// const createObject = () => {
+//   const randomIdIndex = randomInteger(0, idObjects.length);
+//   const randomUrlIndex = randomInteger(0, links.length);
+//   const randomLikeIndex = randomInteger(0, likes.length);
+
+//   return {
+//     id: idObjects[randomIdIndex],
+//     url: links[randomUrlIndex],
+//     description: '',
+//     likes: likes[randomLikeIndex],
+//     comments: '',
+//   };
+// };
+
